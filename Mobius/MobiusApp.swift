89ea7bc8be3aad6b8807647e7bb724a5c2f39c55
Luciano Di Croce <luciano.dicroce@mac.com>
2025-08-10 09:44:55 +0200

    Initial commit: Mobius version 1

---
+++ Mobius/MobiusApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobiusApp.swift
+//  Mobius
+//
+//  Created by Luciano Di Croce on 9/8/25.
+//
+
+import SwiftUI
+
+@main
+struct MobiusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Mobius/Test.swift
@@ -0,0 +1,7 @@
+//
+//  Test.swift
+//  Mobius
+//
+//  Created by Luciano Di Croce on 9/8/25.
+//
+
